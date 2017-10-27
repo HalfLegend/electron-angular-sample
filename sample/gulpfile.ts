81cc1fbd60d076bb5b9ec96e8ab8551c22c2fff0
HalfLegend <HalfLegend@live.com>
2017-10-27 10:28:48 +0800

    更新例程

--- sample/gulpfile.ts
+++ sample/gulpfile.ts
@@ -1,5 +1,5 @@
 
-import gulp = require("gulp");
+import * as gulp from "gulp";
 
 gulp.task("M-ElectronAngular", function () {
     gulp.src(["../../elegular/!(node_modules)/**/*.?(js|ts|js.map|html)", "../../elegular/*.?(js|ts|js.map)"]).pipe(gulp.dest("node_modules/elegular"));
